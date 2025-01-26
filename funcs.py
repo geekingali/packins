bf9d19384c8f95705c0c7077f9806c12c3caf062
ali <geekingali@gmail.com>
2025-01-27 01:16:06 +0330

    add try exept

--- funcs.py
+++ funcs.py
@@ -20,22 +20,38 @@ def logo():
                                                                             
     ''')
 
-inpt = lambda:int(input(Fore.YELLOW + '[enter number] ===> '))
+def inpt():
+    try:
+        return int(input(Fore.YELLOW + '[enter number] ===> '))
+    except:
+        return int(input(Fore.RED + '[Please enter a whole number][to exit : 99] ===> '))
+
+# inpt = lambda:int(input(Fore.YELLOW + '[enter number] ===> '))
 
 menu_render = lambda menu: ''.join(f'{Fore.GREEN}[{i}] : {m}\n' for i, m in enumerate(menu)) + Fore.CYAN + '[99] : Exit \n'
 
-def rund(cmd):
-    c = call(cmd, shell=True)
-    return c
-    
+def run(cmd):
+    try:
+        c = call(cmd, shell=True)
+        return c
+    except KeyboardInterrupt:
+        ec = input(Fore.MAGENTA + '\n installing canceled, are you redy to reinstall[N/y] ===>')
+        print(Fore.RESET)
+        if ec == 'y' or ec == 'Y':
+            system('clear')
+            r = run(cmd)
+            return r
+        else:
+            return 'c'
+
 def install(commands:dict):
     logo()
     logs_code = []
     is_cnt = True
-    print(menu_render([f'install with {i}' for i in commands[like()()].keys()]))
+    print(menu_render([f'install with {i}' for i in commands[like()].keys()]))
     option = inpt()
     print(Fore.RESET)
-    cmds = (commands[like()()][list(commands[like()()].keys())[option]])
+    cmds = (commands[like()][list(commands[like()].keys())[option]])
     system('clear')
     print(Fore.MAGENTA + 'start installing ...' + Fore.RESET)
     for cmd in cmds:
@@ -43,10 +59,13 @@ def install(commands:dict):
             chdir(str(cmd[0].split(' ')[1:])[2:-2].replace(',','').replace('\'','').replace('"',''))
             print(Fore.GREEN + f'installing {cmds.index(cmd) + 1}/{len(cmds) + 1} ...' + Fore.RESET)
             continue
-        r = rund(cmd)
+        r = run(cmd)
         if r == 0:
             print(Fore.GREEN + f'installed {cmds.index(cmd) + 1}/{len(cmds) + 1} ...' + Fore.RESET)
             print(Fore.BLUE + f'installing {cmds.index(cmd) + 2}/{len(cmds) + 1} ...' + Fore.RESET)
+        elif r == 'c':
+            print(Fore.BLUE + 'installing canceled by user')
+            exit(0)
         else:
             ec = input(Fore.RED + 'is eror, continue or exit?[E/c] ==>')
             if ec == 'c' or ec == 'C':
