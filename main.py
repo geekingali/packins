92cbc6d4a04b37387d64fb48a8f549f46a96d010
ali <geekingali@gmail.com>
2025-01-28 19:52:11 +0330

    read install command with json file

--- main.py
+++ main.py
@@ -4,23 +4,38 @@ from funcs import menu_render, logo, inpt, inpterr, install
 def main():
     last_menu = menu_main
     is_option = False
+    noterr = True
     while True:
-        logo()
-        ex = True
-        try:
-            if type(last_menu) == dict: print(menu_render(last_menu.keys()))
-            if type(last_menu) == list: print(menu_render(last_menu))
-            option = inpt()
-        except Exception as e:
-            option = inpterr()
+        if noterr:
+            logo()
+            ex = True
+            try:
+                if type(last_menu) == dict: print(menu_render(last_menu.keys()))
+                if type(last_menu) == list: print(menu_render(last_menu))
+                option = inpt()
+            except Exception as e:
+                option = inpterr()
+                if option == 99: break
             if option == 99: break
-        if option == 99: break
         if type(last_menu) == dict:
-            last_menu = last_menu[list(last_menu.keys())[option]]
+            try:
+                last_menu = last_menu[list(last_menu.keys())[option]]
+            except Exception as e:
+                option = inpterr()
+                if option == 99: break
+                noterr = False
+            else: noterr = True
         if type(last_menu) == list:
             logo()
             print(menu_render(last_menu))
             option = inpt()
-            install(last_menu[option])
+            if option == 99: break
+            try:
+                install(last_menu[option])
+            except:
+                option = inpterr()
+                if option == 99: break
+                noterr = False
+            else: noterr == True
 
 if __name__ == '__main__': main()
