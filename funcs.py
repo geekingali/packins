f9ae90dba02a3c93bc44d3e8b0a5a7d6ac57ca57
ali <geekingali@gmail.com>
2025-01-28 23:53:42 +0330

    read menu with json and add try except and fix bugs and ui copleted.

--- funcs.py
+++ funcs.py
@@ -5,6 +5,10 @@ from subprocess import call
 from sys import executable, argv
 from json import load
 
+with open('menu.json') as f:
+    menu_main = load(f)
+
+
 def logo():
     system('clear')
     init()
@@ -43,10 +47,12 @@ def inpt():
     except:
         return inpterr()
 
-#menu_render = lambda menu: ''.join(f'{Fore.LIGHTGREEN_EX}[{i}] : {m}\n' for i, m in enumerate(menu)) + Fore.LIGHTCYAN_EX + '[99] : Exit \n'
-def menu_render(menu):
-    if menu == []: return Fore.LIGHTMAGENTA_EX + '[!] : installation method not found.\n\n' + Fore.LIGHTGREEN_EX + '[1] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n'
-    return ''.join(f'{Fore.LIGHTGREEN_EX}[{i}] : {m}\n' for i, m in enumerate(menu)) + Fore.LIGHTCYAN_EX + '[99] : Exit \n'
+def menu_render(menu,ismainmenu=False):
+    if menu == []: return Fore.LIGHTMAGENTA_EX + '[!] : installation method not found.\n\n' + Fore.LIGHTGREEN_EX + '\n[98] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n'
+    ismenu = ''
+    if not ismainmenu:
+        ismenu = Fore.LIGHTMAGENTA_EX + '\n[98] : back to menu.\n'
+    return ''.join(f'{Fore.LIGHTGREEN_EX}[{i}] : {m}\n' for i, m in enumerate(menu)) + ismenu + Fore.LIGHTCYAN_EX + '[99] : Exit \n'
 
 def run(cmd):
     try:
@@ -62,59 +68,32 @@ def run(cmd):
         else:
             return 'c'
 
-# def install(commands:dict):
-#     logo()
-#     logs_code = []
-#     is_cnt = True
-#     try:
-#         print(menu_render([f'install with {i}' for i in commands[getlike()].keys()]))
-#     except:
-#         print(Fore.LIGHTMAGENTA_EX + '==> installation method not found.\n\n' + Fore.LIGHTGREEN_EX + '[1] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n')
-#     option = inpt()
-#     if option == 1:execv(executable,['python'] + argv)
-#     try:
-#         cmds = (commands[getlike()][list(commands[getlike()].keys())[option]])
-#     except :
-#         if option == 99: exit(0)
-#     system('clear')
-#     print(Fore.LIGHTMAGENTA_EX + 'start installing ...' + Fore.RESET)
-#     for cmd in cmds:
-#         if 'cd' in str(cmd):
-#             chdir(str(cmd[0].split(' ')[1:])[2:-2].replace(',','').replace('\'','').replace('"',''))
-#             print(Fore.LIGHTGREEN_EX + f'installing {cmds.index(cmd) + 1}/{len(cmds) + 1} ...' + Fore.RESET)
-#             continue
-#         r = run(cmd)
-#         if r == 0:
-#             print(Fore.LIGHTGREEN_EX + f'installed {cmds.index(cmd) + 1}/{len(cmds) + 1} ...' + Fore.RESET)
-#             print(Fore.LIGHTBLUE_EX + f'installing {cmds.index(cmd) + 2}/{len(cmds) + 1} ...' + Fore.RESET)
-#         elif r == 'c':
-#             print(Fore.LIGHTBLUE_EX + 'installing canceled by user')
-#             exit(0)
-#         else:
-#             print(Fore.LIGHTRED_EX + '\neror in install\n')
-#             if input(f'\n{Fore.LIGHTYELLOW_EX}Want to back menu(Y/n) : ') in {'n','N'}: exit(0)
-#             else: execv(executable,['python'] + argv)
-#     else:
-#         print(Fore.LIGHTMAGENTA_EX + 'completed install')
-#     if input(f'\n{Fore.LIGHTYELLOW_EX}Want to back menu(Y/n) : ') in {'n','N'}: exit(0)
-#     else: execv(executable,['python'] + argv)
-
-def install(name:str):
-    with open('install.json') as f:
-        commands = (load(f)[name])
-    logo()
-    logs_code = []
-    is_cnt = True
+def install(name:str,option=0,noterr=True):
     try:
-        print(menu_render([f'install with {i}' for i in commands[getlike()].keys()]))
+        with open('install.json') as f:
+            commands = (load(f)[name])
     except:
-        print(Fore.LIGHTMAGENTA_EX + '==> installation method not found.\n\n' + Fore.LIGHTGREEN_EX + '[1] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n')
-    option = inpt()
-    if option == 1:execv(executable,['python'] + argv)
+            print(Fore.LIGHTMAGENTA_EX + '==> installation method not found.\n\n' + Fore.LIGHTMAGENTA_EX + '\n[98] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n')
+    if noterr:
+        logo()
+        logs_code = []
+        is_cnt = True
+        try:
+            print(menu_render([f'install with {i}' for i in commands[getlike()].keys()]))
+        except:
+            print(Fore.LIGHTMAGENTA_EX + '==> installation method not found.\n\n' + Fore.LIGHTMAGENTA_EX + '\n[98] : back to menu.\n' + Fore.LIGHTCYAN_EX + '[99] : Exit \n')
+        option = inpt()
+        if option == 98:execv(executable,['python'] + argv)
     try:
         cmds = (commands[getlike()][list(commands[getlike()].keys())[option]])
-    except :
+    except:
         if option == 99: exit(0)
+        else:
+            noterr = False
+            option = inpterr()
+            if option == 99: exit(0)
+            install(name,option,False)
+    else: noterr = True
     system('clear')
     print(Fore.LIGHTMAGENTA_EX + 'start installing ...' + Fore.RESET)
     for cmd in cmds:
@@ -128,7 +107,8 @@ def install(name:str):
             print(Fore.LIGHTBLUE_EX + f'installing {cmds.index(cmd) + 2}/{len(cmds) + 1} ...' + Fore.RESET)
         elif r == 'c':
             print(Fore.LIGHTBLUE_EX + 'installing canceled by user')
-            exit(0)
+            if input(f'\n{Fore.LIGHTYELLOW_EX}Want to back menu(Y/n) : ') in {'n','N'}: exit(0)
+            else: execv(executable,['python'] + argv)
         else:
             print(Fore.LIGHTRED_EX + '\neror in install\n')
             if input(f'\n{Fore.LIGHTYELLOW_EX}Want to back menu(Y/n) : ') in {'n','N'}: exit(0)
--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-from menu import menu_main
-from funcs import menu_render, logo, inpt, inpterr, install
+from funcs import menu_render, logo, inpt, inpterr, install, menu_main, execv, executable, argv
 
 def main():
     last_menu = menu_main
     is_option = False
     noterr = True
+    ismainmenu = True
     while True:
         if noterr:
             logo()
             ex = True
             try:
-                if type(last_menu) == dict: print(menu_render(last_menu.keys()))
-                if type(last_menu) == list: print(menu_render(last_menu))
+                if type(last_menu) == dict: print(menu_render(last_menu.keys(),ismainmenu))
+                if type(last_menu) == list: print(menu_render(last_menu,ismainmenu))
                 option = inpt()
             except Exception as e:
                 option = inpterr()
                 if option == 99: break
+                if option == 98:execv(executable,['python'] + argv)
             if option == 99: break
+            if option == 98:execv(executable,['python'] + argv)
         if type(last_menu) == dict:
             try:
                 last_menu = last_menu[list(last_menu.keys())[option]]
             except Exception as e:
                 option = inpterr()
                 if option == 99: break
+                if option == 98:execv(executable,['python'] + argv)
                 noterr = False
             else: noterr = True
         if type(last_menu) == list:
             logo()
-            print(menu_render(last_menu))
+            print(menu_render(last_menu,ismainmenu))
             option = inpt()
             if option == 99: break
+            if option == 98:execv(executable,['python'] + argv)
             try:
                 install(last_menu[option])
-            except:
-                option = inpterr()
-                if option == 99: break
+            except KeyboardInterrupt: exit(0)
+            except Exception as e:
+                # option = inpterr()
+                if option == 99: exit(0)
+                if option == 98:execv(executable,['python'] + argv)
                 noterr = False
             else: noterr == True
+        ismainmenu = False
 
 if __name__ == '__main__': main()
